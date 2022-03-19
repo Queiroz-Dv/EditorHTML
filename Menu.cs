71c1da84ca97e4ab36b38fd23ad7150cf466db1c
Queiroz <kalifor_dbel@hotmail.com>
2022-03-19 11:15:05 -0300

    Implementando modelo do menu

--- Menu.cs
+++ Menu.cs
@@ -9,6 +9,36 @@ namespace EditorHtml
       Console.BackgroundColor = ConsoleColor.DarkGray;
       // Foreground é a cor da letra 
       Console.ForegroundColor = ConsoleColor.White;
+
+      DrawScreen();
+    }
+
+    public static void DrawScreen()
+    {
+      //Top Base
+      Console.WriteLine("+");
+      for (int i = 0; i < 30; i++)
+        Console.WriteLine("-");
+
+      Console.WriteLine("+");
+      Console.WriteLine("\n");
+
+      //Colums
+      for (int lines = 0; lines < 10; lines++)
+      {
+        Console.WriteLine("|");
+        for (int i = 0; i < 30; i++)
+          Console.WriteLine(" ");
+
+        Console.WriteLine("|");
+      }
+      // Top Bottom
+      Console.WriteLine("+");
+      for (int i = 0; i < 30; i++)
+        Console.WriteLine("-");
+
+      Console.WriteLine("+");
+      Console.WriteLine("\n");
     }
   }
 }
